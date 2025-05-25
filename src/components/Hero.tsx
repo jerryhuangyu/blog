ac238ef07e9754f9c6c391329ae9801623e7c619
jerryhuangyu <jerry0932619628@gmail.com>
2025-05-25 14:14:02 +0800

    feat: Add blogs marquee for home page

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Ripple } from "./magicui/ripple";
+
+const Hero = () => {
+	return (
+		<main>
+			<div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+				<p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
+					Huang Yu - Blogs
+				</p>
+				<p className="text-gray-500">Hi 👋, I'm Web Developer from Taiwan 🇹🇼</p>
+				<Ripple mainCircleOpacity={0.3} mainCircleSize={500} numCircles={10} />
+			</div>
+		</main>
+	);
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,13 @@
+import Hero from "@/components/Hero";
+import MarqueeBlogs from "@/components/MarqueeBlogs";
 import Layout from "@theme/Layout";
 import React from "react";
-import { Ripple } from "../components/magicui/ripple";
 
 export default function Home() {
 	return (
 		<Layout>
-			<main>
-				<div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
-					<p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
-						Huang Yu - Blogs
-					</p>
-					<Ripple
-						mainCircleOpacity={0.3}
-						mainCircleSize={500}
-						numCircles={10}
-					/>
-				</div>
-			</main>
+			<Hero />
+			<MarqueeBlogs />
 		</Layout>
 	);
 }
